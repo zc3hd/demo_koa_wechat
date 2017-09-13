37cd7b1a3b40dedd79e46d1a7eb2fd83e2b08f63
cc <123@example.com>
2017-09-14 00:12:22 +0800

    tz-my 项样式 大概完成

--- webapp/modules/sdk/tz_test/modules/Main/index.js
+++ webapp/modules/sdk/tz_test/modules/Main/index.js
@@ -22,18 +22,40 @@
       // 
       me.event();
     },
-    // 
+    // 事件
     event: function() {
       var me = this;
+      // 菜单弹出
       me.list();
+      // 个人信息
+      me.my();
     },
+    // 菜单
     list: function() {
       var me = this;
       $('#list').on('click', function() {
+        // 先看my是否打开
+        var my_key = $('#my').attr('key');
+        // 已打开
+        if (my_key==1) {
+          me.my_off_style();
+        }
+
         var key = $('#list').attr('key');
         // 关闭状态--要打开
         if (key == 0) {
-          $('#list').attr('key', 1);
+          me.list_on_style();
+        }
+        // 打开状态--要关闭
+        else if (key == 1) {
+          me.list_off_style();
+        }
+      });
+    },
+    // 菜单项要打开的样式
+    list_on_style:function(){
+      var me  = this;
+      $('#list').attr('key', 1);
 
           // 菜单nav
           $('#list').css({
@@ -41,6 +63,7 @@
             backgroundColor: '#0000CD',
             borderRight: 'none'
           });
+
           $('#info').css({
             width: '57%',
           });
@@ -50,10 +73,11 @@
             left: '0',
             boxShadow: '3px 0px 10px #000'
           });
-        }
-        // 打开状态--要关闭
-        else if (key == 1) {
-          $('#list').attr('key', 0);
+    },
+    // 菜单项要关闭的样式
+    list_off_style:function(){
+      var me = this;
+      $('#list').attr('key', 0);
 
           // 菜单nav
           $('#list').css({
@@ -70,9 +94,74 @@
             left: '-30%',
             boxShadow: '0px 0px 0px #000'
           });
+    },
+    // 个人信息
+    my: function() {
+      var me = this;
+      $('#my').on('click', function() {
+        // 先看list是否打开
+        var list_key = $('#list').attr('key');
+        // 已打开
+        if (list_key==1) {
+          me.list_off_style();
+        }
+
+        var key = $('#my').attr('key');
+        // 关闭状态--要打开
+        if (key == 0) {
+          me.my_on_style();
+
+        }
+        // 打开状态--要关闭
+        else if (key == 1) {
+          me.my_off_style();
         }
       });
     },
+    // my项要打开的样式
+    my_on_style:function(){
+      var me = this;
+      $('#my').attr('key', 1);
+
+          // my--按钮
+          $('#my').css({
+            width: '65%',
+            backgroundColor: '#0000CD',
+            borderLeft: 'none'
+          });
+
+          $('#info').css({
+            width: '22%',
+          });
+
+          // my--详情
+          $('#nav_my').css({
+            bottom: '0',
+            boxShadow: '3px 0px 10px #000'
+          });
+    },
+    // my项要关闭的样式
+    my_off_style:function(){
+      var me =this;
+      $('#my').attr('key', 0);
+
+          // my--按钮
+          $('#my').css({
+            width: '13%',
+            backgroundColor: 'rgb(28, 27, 32)',
+            borderLeft: '1px solid #0000CD'
+          });
+
+          $('#info').css({
+            width: '74%',
+          });
+
+          // 菜单详情
+          $('#nav_my').css({
+            bottom: '-40%',
+            boxShadow: '0px 0px 0px #000'
+          });
+    }
   };
   conf.module["Main_html"] = Main_html;
 })(jQuery, window);
