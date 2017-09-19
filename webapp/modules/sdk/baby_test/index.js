245d06540ff2685e7e1053a4ff6ebeea822144a5
armincc <343371169@qq.com>
2017-09-19 11:51:21 +0800

    bb-test nav 分开

--- webapp/modules/sdk/baby_test/index.js
+++ webapp/modules/sdk/baby_test/index.js
@@ -15,26 +15,35 @@
     // 事件
     event: function() {
       var me = this;
+      // 轮播图
       me._swiper();
-      // 广告
+
+      // 实时广告
       me._adv({
         parent: '#advp',
         son: '#adv'
       });
 
-      // 抽奖
+      // 支线任务
       setTimeout(function(argument) {
         me._adv({
           parent: '#lottery_p',
           son: '#lottery_s'
         });
-      }, 2000)
+      }, 2000);
 
+      var layer_load = layer.load(0, { shade: 0.5 });
       // 导航
       me._nav();
+      setTimeout(function(argument) {
+        // 加载
+        me._nav_load('main', layer_load);
+      }, 2000)
 
-      me._list();
+
+
     },
+    // -----------------------------------forever
     // 轮播图
     _swiper: function(argument) {
       var me = this;
@@ -110,18 +119,52 @@
       var me = this;
       $('#app>.nav>div').on('click', function(e) {
         // ------------css
-        var key = $(e.currentTarget).hasClass('active');
-        if (key) {
+        var class_key = $(e.currentTarget).hasClass('active');
+        var attr_key = $(e.currentTarget).attr('key');
+
+        if (class_key) {
           return
         }
         $('#app>.nav>div').removeClass('active');
         $(e.currentTarget).addClass('active');
-        // -------------js
 
+        // -------------js
+        var layer_load = layer.load(0, { shade: 0.5 });
+        me._nav_load(attr_key,layer_load);
       })
     },
+    // 菜单加载
+    _nav_load: function(key, layer_load) {
+      var me = this;
+      $('#main').load(`./tpl/${key}.html`, function() {
+        // 主页
+        if (key == 'main') {
+          // 数据列表
+          me._main(layer_load);
+        }
+        // 排行列表
+        else if (key == 'level') {
+          me._level(layer_load);
+        }
+        // 赛事说明
+        else if (key == 'info') {
+          me._info(layer_load);
+        }
+        // 商家合作
+        else if (key == 'reg') {
+          me._reg(layer_load);
+        }
+      });
+    },
+    // -----------------------------------主页展示
     // 数据展示
-    _list: function() {
+    _main: function(layer_load) {
+      var me = this;
+      // 展示列表
+      me._mian_list(layer_load);
+    },
+    // 展示列表
+    _mian_list:function  (layer_load) {
       var me = this;
       var width = $('#app>.main>.list ').width();
       //主要部分
@@ -129,18 +172,36 @@
         // 间距
         gutter: 3,
         // 宽度
-        width: (width-50)/2,
+        width: (width - 80) / 2,
         animate: true,
         animationOptions: {
           speed: 150,
           duration: 400,
           // 预先设置完的图片加载完的回调函数
           complete: function(data) {
-            console.log("dwtedx个人博客 - dwtedx.com");
+            // 加载回来的数据
+            // console.log(data);
+            layer.close(layer_load);
           },
         },
       });
     },
+    // -----------------------------------排行信息
+    _level:function  (layer_load) {
+      var me = this;
+      layer.close(layer_load);
+    },
+    // -----------------------------------赛事说明
+    _info:function  (layer_load) {
+      var me = this;
+      layer.close(layer_load);
+    },
+    // -----------------------------------商家入口
+    _reg:function  (layer_load) {
+      var me = this;
+      layer.close(layer_load);
+    },
+
   };
   conf.module["Main_html"] = Main_html;
 })(jQuery, window);
